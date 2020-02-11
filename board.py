0b3d0cc7a4672aa056702a197fe663583c7f3cf1
SaurabhPethani <spethani28@gmail.com>
2020-02-12 00:31:01 +0530

    version1.2

--- board.py
+++ board.py
@@ -25,17 +25,47 @@ for i in range(8):
     labelReference.append(ls)
 
 # Printing Black Pieces
-# for i in range(8):
-#     labelReference[0][i]['text'] = blackList[i]
 for i in range(8):
-    labelReference[1][i]['image'] = tkinter.PhotoImage(file = './soldier.png')
+    labelReference[0][i]['text'] = blackList[i]
+for i in range(8):
+    labelReference[1][i]['text'] = '\u265F'
 
 # Printing White Pieces
-# for i in range(8):
-#     labelReference[7][i]['image'] = whiteList[i]
-# for i in range(8):
-#     labelReference[6][i]['text'] = '\u2659'
+for i in range(8):
+    labelReference[7][i]['text'] = whiteList[i]
+for i in range(8):
+    labelReference[6][i]['text'] = '\u2659'
 
+class MetaLabel:
+    def __init__(self, position, character):
+        self.position = position
+        if character == 56:
+            self.character = "black:eleph"
+        elif character ==58:
+            self.character = 'black:horse'
+        elif character ==57:
+            self.character = 'black:camel'
+        elif character ==55:
+            self.character = 'black:king'
+        elif character ==54:
+            self.character = 'black:queen'
+        elif character ==59:
+            self.character = 'black:soldier'
+        elif character == 50:
+            self.character = "white:eleph"
+        elif character ==52:
+            self.character = 'white:horse'
+        elif character ==51:
+            self.character = 'white:camel'
+        elif character ==49:
+            self.character = 'white:king'
+        elif character ==48:
+            self.character = 'white:queen'
+        elif character ==53:
+            self.character = 'white:soldier'
+        
+        
+        
 # Bind Every label with double click mouse event
 metaLabel={}
 index = 1
@@ -48,8 +78,18 @@ def onDoubleClick(event):
     labelName = str(event.widget)
     if event.widget['text'] is not '':
         val = labelName[7:]
-        print(str(event.widget['text']))
-        print(metaLabel[int(val)])
+        print(str(event.widget['text'].encode('utf')[2])[1:])
+        char = int(str(event.widget['text'].encode('utf')[2])[1:])
+        print(len(event.widget['text'].encode('utf')))
+        pos = ''
+        if val is '':
+            print(metaLabel[1])
+            pos = metaLabel[1]
+        else:
+            print(metaLabel[int(val)])
+            pos = metaLabel[int(val)]
+        metaObject = MetaLabel(pos, char)
+        print("meta Object ",metaObject.character)
 for labelList in labelReference:
     for label in labelList:
         label.bind("<Double-Button-1>", onDoubleClick)
